8af9e066c192d6370b03ddf3345cccebb4d6a727
Ben Anhalt <anhalt@ku.edu>
2019-09-24 13:17:03 -0500

    separate matchRows into module.

--- src/Main.purs
+++ src/Main.purs
@@ -2,36 +2,30 @@ module Main where
 
 import Prelude
 
-import Data.Array (uncons, mapWithIndex)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
-import Data.NonEmpty (foldl1, (:|))
 import Data.Traversable (for_)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (logShow)
+import MatchRows (ColumnType(..), MappingItem, matchRows)
 import MySQL.Connection (closeConnection, createConnection, defaultConnectionInfo, query_)
-import SQL (Alias, JoinExpr, ScalarExpr, SelectExpr(..), SelectTerm(..), and, as, equal, isNull, join, leftJoin, nullIf, or, plus, query, (..))
+import SQL (SelectExpr(..), equal, (..))
 
 main :: Effect Unit
 main = do
-  logShow matchRows
+  logShow matchRows_
   conn <- createConnection $ defaultConnectionInfo {database = "uconnverts", password = "Master", user = "Master"}
   launchAff_ do
-    result :: Array {localityid :: Int, rownumber :: Int} <- query_ (show matchRows) conn
+    result :: Array {localityid :: Int, rownumber :: Int} <- query_ (show matchRows_) conn
     for_ result \r -> do
       liftEffect $ logShow r
     liftEffect $ closeConnection conn
 
 
-type MappingItem = {columnName :: String, columnType :: ColumnType, id :: Int}
-
-data ColumnType = StringType | DoubleType | IntType | DecimalType
-
-
-mappingItems' :: Array MappingItem
-mappingItems' = [ {columnName: "shortName", columnType: StringType, id: 1907 }
+mappingItems :: Array MappingItem
+mappingItems = [ {columnName: "shortName", columnType: StringType, id: 1907 }
                , {columnName: "localityName", columnType: StringType, id: 1908 }
                , {columnName: "Text2", columnType: StringType, id: 1913 }
                , {columnName: "verbatimElevation", columnType: StringType, id: 1930 }
@@ -43,51 +37,8 @@ mappingItems' = [ {columnName: "shortName", columnType: StringType, id: 1907 }
                , {columnName: "longitude1", columnType: DecimalType, id: 1941 }
                ]
 
-matchRows :: SelectExpr
-matchRows = matchRows' mappingItems' (Table "locality") (\t -> Just $ (t .. "disciplineid") `equal` wrap "3") "localityid"
-
-matchRows' :: Array MappingItem -> SelectExpr -> (Alias -> Maybe ScalarExpr) -> String -> SelectExpr
-matchRows' mappingItems matchTable whereExpr idCol =
-  query [SelectTerm $ t .. idCol, SelectTerm $ wb .. "rownumber"] (matchTable `as` t) joinWB (whereExpr t)
-  where
-    (t :: Alias) = wrap "t"
-    (wb :: Alias) = wrap "wb"
-    joinWB = case uncons $ map compValues mappingItems of
-      Just { head: c, tail: cs } ->  [join (rowsFromWB mappingItems) wb $ Just (foldl1 and (c :| cs))]
-      Nothing -> []
-
-rowsFromWB :: Array MappingItem -> SelectExpr
-rowsFromWB mappingItems = query
-                          (mapWithIndex makeSelectWB mappingItems <> [SelectTerm $ r .. "rownumber"])
-                          (Table "workbenchrow" `as` r)
-                          (mapWithIndex makeJoinWB mappingItems)
-                          (Just $ (r .. "workbenchid") `equal` wrap "27")
-  where (r :: Alias) = wrap "r"
-
-compValues :: MappingItem -> ScalarExpr
-compValues item = case item.columnType of
-  StringType -> (v1 `equal` v2) `or` (isNull v1 `and` isNull v2) `or` (isNull v1 `and` (v2 `equal` wrap "''"))
-  otherwise -> (v1 `equal` v2) `or` (isNull v1 `and` isNull v2)
-  where v1 = wrap "t" .. item.columnName
-        v2 = wrap "wb" .. item.columnName
-
-makeJoinWB :: Int -> MappingItem -> JoinExpr
-makeJoinWB i item = leftJoin (Table "workbenchdataitem") dataItem $ Just $
-                    ((dataItem .. "workbenchrowid") `equal` (r .. "workbenchrowid")) `and`
-                    ((dataItem .. "workbenchtemplatemappingitemid") `equal` (wrap $ show item.id))
-  where (dataItem :: Alias) = wrap ("c" <> (show i))
-        (r :: Alias) = wrap "r"
-
-parseValue :: ColumnType -> ScalarExpr -> ScalarExpr
-parseValue StringType value = value
-parseValue DoubleType value =  nullIf value (wrap "''") `plus` (wrap "0.0")
-parseValue IntType value = nullIf value (wrap "''") `plus` (wrap "0")
-parseValue DecimalType value = nullIf value (wrap "''")
-
-makeSelectWB :: Int -> MappingItem -> SelectTerm
-makeSelectWB i item = SelectAs item.columnName (parseValue item.columnType value)
-  where value = (wrap $ "c" <> (show i)) .. "celldata"
-
+matchRows_ :: SelectExpr
+matchRows_ = matchRows mappingItems (Table "locality") (\t -> Just $ (t .. "disciplineid") `equal` wrap "3") "localityid"
 
 --   "select\n" <> (intercalate ",\n" $ mapWithIndex makeSelectWB mappingItems) <> """,
 -- now()                                timestampcreated,
---
+++ src/MatchRows.purs
@@ -0,0 +1,59 @@
+module MatchRows (MappingItem, ColumnType(..), matchRows) where
+
+import Prelude
+
+import Data.Array (uncons, mapWithIndex)
+import Data.Maybe (Maybe(..))
+import Data.Newtype (wrap)
+import Data.NonEmpty (foldl1, (:|))
+import SQL (Alias, JoinExpr, ScalarExpr, SelectExpr(..), SelectTerm(..), and, as, equal, isNull, join, leftJoin, nullIf, or, plus, query, (..))
+
+
+type MappingItem = {columnName :: String, columnType :: ColumnType, id :: Int}
+
+data ColumnType = StringType | DoubleType | IntType | DecimalType
+
+
+matchRows :: Array MappingItem -> SelectExpr -> (Alias -> Maybe ScalarExpr) -> String -> SelectExpr
+matchRows mappingItems matchTable whereExpr idCol =
+  query [SelectTerm $ t .. idCol, SelectTerm $ wb .. "rownumber"] (matchTable `as` t) joinWB (whereExpr t)
+  where
+    (t :: Alias) = wrap "t"
+    (wb :: Alias) = wrap "wb"
+    joinWB = case uncons $ map compValues mappingItems of
+      Just { head: c, tail: cs } ->  [join (rowsFromWB mappingItems) wb $ Just (foldl1 and (c :| cs))]
+      Nothing -> []
+
+rowsFromWB :: Array MappingItem -> SelectExpr
+rowsFromWB mappingItems = query
+                          (mapWithIndex makeSelectWB mappingItems <> [SelectTerm $ r .. "rownumber"])
+                          (Table "workbenchrow" `as` r)
+                          (mapWithIndex makeJoinWB mappingItems)
+                          (Just $ (r .. "workbenchid") `equal` wrap "27")
+  where (r :: Alias) = wrap "r"
+
+compValues :: MappingItem -> ScalarExpr
+compValues item = case item.columnType of
+  StringType -> (v1 `equal` v2) `or` (isNull v1 `and` isNull v2) `or` (isNull v1 `and` (v2 `equal` wrap "''"))
+  otherwise -> (v1 `equal` v2) `or` (isNull v1 `and` isNull v2)
+  where v1 = wrap "t" .. item.columnName
+        v2 = wrap "wb" .. item.columnName
+
+makeJoinWB :: Int -> MappingItem -> JoinExpr
+makeJoinWB i item = leftJoin (Table "workbenchdataitem") dataItem $ Just $
+                    ((dataItem .. "workbenchrowid") `equal` (r .. "workbenchrowid")) `and`
+                    ((dataItem .. "workbenchtemplatemappingitemid") `equal` (wrap $ show item.id))
+  where (dataItem :: Alias) = wrap ("c" <> (show i))
+        (r :: Alias) = wrap "r"
+
+parseValue :: ColumnType -> ScalarExpr -> ScalarExpr
+parseValue StringType value = value
+parseValue DoubleType value =  nullIf value (wrap "''") `plus` (wrap "0.0")
+parseValue IntType value = nullIf value (wrap "''") `plus` (wrap "0")
+parseValue DecimalType value = nullIf value (wrap "''")
+
+makeSelectWB :: Int -> MappingItem -> SelectTerm
+makeSelectWB i item = SelectAs item.columnName (parseValue item.columnType value)
+  where value = (wrap $ "c" <> (show i)) .. "celldata"
+
+
